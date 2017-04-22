6d8b5592d2fc500fa7a464e8e2fa41c11b27f165
Prasanna Kumar <jpkbeddu@gmail.com>
2017-04-22 14:59:51 +0530

    Adds material-ui and its dependencies

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import logo from './logo.svg';
+import 'roboto-fontface';
 import './App.css';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Keep App</h2>
+      <MuiThemeProvider>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to Keep App</h2>
+          </div>
+          <p className="App-intro">
+            This is a Google Keep clone app build using
+            {' '}
+            <code>create-react-app</code>
+            .
+          </p>
         </div>
-        <p className="App-intro">
-          This is a Google Keep clone app build using <code>create-react-app</code>.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
