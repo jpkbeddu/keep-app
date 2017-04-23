e2f9a19bc727f8e2721780cadcdb8cac2b98673f
Prasanna Kumar <jpkbeddu@gmail.com>
2017-04-23 10:50:50 +0530

    Updates Appbar to look like Google-keep

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { orange300, darkBlack } from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import 'roboto-fontface';
 import './App.css';
 import Appbar from './components/Appbar';
@@ -10,6 +12,13 @@ import WelcomeNote from './components/WelcomeNote';
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
+const muiTheme = getMuiTheme({
+  appBar: {
+    color: orange300,
+    textColor: darkBlack
+  }
+});
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -17,10 +26,11 @@ class App extends Component {
       isLogged: false
     };
   }
+
   render() {
     const { isLogged } = this.state;
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={muiTheme}>
         <div>
           <Appbar logged={isLogged} />
           <WelcomeNote />
